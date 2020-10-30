e7ff9058ec772adbcac89253e2ded86d045c9e50
Vince Scacchitti <VScacchitti24@gmail.com>
2020-10-30 06:00:51 -0400

    updated schema and seeds for the dtatbase.

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS burgertime_DB;
+
+CREATE DATABASE burgertime_DB;
+
+USE burgertime_DB;
+
+CREATE TABLE burgers (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    burger_name VARCHAR(255) NOT NULL,
+    eat_burger BOOLEAN DEFAULT false
+);
+
+SELECT * FROM burgers;
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgertime_DB;
+
+INSERT INTO burgers (burger_name, eat_burger)
+VALUES ("Cheese Burger", false),
+("Bacon Burger", false),
+("BBQ Burger", true),
+("Spciy Slaw Burger", false),
+("Turkey Burger", false)
