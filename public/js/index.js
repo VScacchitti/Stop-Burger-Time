88957b56d694117b2b92b3e6a388be1adede1cb6
Vince Scacchitti <VScacchitti24@gmail.com>
2020-11-01 08:53:48 -0500

    created front-end ajax class to the api.

--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,46 @@
 
-
-// Get info from the form
-$('#form').on("submit", function(event){
-    //stops page form reloading automatically
-    event.preventDefault();
-
-    //Creating a new Burger
-    let newBurger = {
-        burger_name : $('#inputBurger').val().trim()
-    }
-    console.log(newBurger)
-
-
-  $.ajax("/", {
-    method : POST,
-    data: newBurger
-}).then(function(){
-    location.reload();
-})
-});
+$(function() {
+    $(".change-eaten").on("click", function(event) {
+      var id = $(this).data("id");
+      var newEaten = $(this).data("neweaten");
+  
+      var newEatenState = {
+        eaten: newEaten
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newEatenState
+      }).then(
+        function() {
+          console.log("changed eaten to", newEaten);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      let newBurger = {
+        name: $("#burger").val().trim(),
+        eaten: $("[name=eaten]:checked").val().trim()
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new Burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
+  
