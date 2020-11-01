cd82683ae1e1335994cb53983b946e89264446aa
Vince Scacchitti <VScacchitti24@gmail.com>
2020-11-01 07:19:59 -0500

    added an on submit function to grab the information from the burger form.

---
+++ public/js/index.js
@@ -0,0 +1,13 @@
+
+
+// Get info from the form
+$('#form').on("submit", function(event){
+    //stops page form reloading automatically
+    event.preventDefault();
+
+    //Creating a new Burger
+    let newBurger = {
+        burger_name : $('#inputBurger').val().trim()
+    }
+    console.log(newBurger)
+})
