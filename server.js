8bfd0d86404e73cdda1cb5d4bbd6e5cddc920b5e
Vince Scacchitti <VScacchitti24@gmail.com>
2020-11-01 08:18:04 -0500

    server ios completed and listening.

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+//imports routes from controller
+const routes = require("./controlllers/burger-time-controller");
+
+
+//Set up Express app
+
+const app = express();
+//process.env lets the port be set by Heroku
+const PORT = process.env.PORT || 8080;
+
+//Set up middleware to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//static directory
+app.use(express.static("public"));
+
+//Using the contorller
+app.use(routes)
+
+//Sets Handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+
+app.listen(PORT, function(){
+    console.log(`Sever is listening on: http://localhost:${PORT}`)
+  })
